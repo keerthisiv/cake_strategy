664459494908cc09643c9843d1a1b6be44180c9f
keerthisiv <keerthisiv@gmail.com>
2014-10-24 19:57:22 -0400

    strategy for 3 and 4 cakes left

--- main.rb
+++ main.rb
@@ -60,15 +60,20 @@ def log_winner(game_result)
 end 
 
 
-log_winner Game(5, true)
-log_winner Game(7, true)
-log_winner Game(12, true)
-log_winner Game(23, true)
+def debugging_check
+  log_winner Game(5, true)
+  log_winner Game(7, true)
+  log_winner Game(12, true)
+  log_winner Game(23, true)
+end
 
-#we_won_counter =0
-#(0..10).each do 
-#  result = Game((1..50).to_a.sample)
-#  we_won_counter += result ? 1 : 0 
-#end
-#
-#puts "we won this # times: #{we_won_counter}"
+def statistical_check
+  we_won_counter =0
+  (0..1000).each do 
+    result = Game((1..50).to_a.sample)
+    we_won_counter += result ? 1 : 0 
+  end
+  puts "we won this # times: #{we_won_counter}"
+end
+
+statistical_check
--- winning_player.rb
+++ winning_player.rb
@@ -4,6 +4,10 @@ class WinningPlayer < Player
     choices = [1,2,3]
     if cakes == 3 && last != 2
       choice = 2
+    elsif cakes == 4 && last != 3
+      choice = 3
+    elsif cakes == 4 && last == 3
+      choice = 2
     else
       choice = choices.reject { |i| i == last }.first
     end
